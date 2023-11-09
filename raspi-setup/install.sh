a4402b5174227a9eccdf37ff5a3264de0300357a
Rudy Norff <rudy@mactunes.de>
2023-11-09 10:05:54 +0100

    fixes to mqtt service installation

--- raspi-setup/install.sh
+++ raspi-setup/install.sh
@@ -2,26 +2,35 @@
 
 # INSTALL EVERYTHING THAT IS NEEDED
 sudo apt update && sudo apt upgrade
-sudo apt install -y mosquitto mosquitto-clients
-sudo systemctl enable mosquitto
-sudo systemctl start mosquitto
-# TODO INSTALL cargo/rustc/etc
 
+echo "Enabling MosQuiTTo on system start"
+sudo apt install -y mosquitto mosquitto-clients
 sudo touch /etc/mosquitto/conf.d/local.conf
 sudo echo "listener 1883" >> /etc/mosquitto/conf.d/local.conf
 sudo echo "allow_anonymous true" >> /etc/mosquitto/conf.d/local.conf
-sudo systemctl restart mosquitto
+sudo systemctl enable mosquitto
+sudo systemctl start mosquitto # start the service directly so we don't have to reboot
 
-# MAKE THE PI HAVE A STATIC IP ADDRESS ON ITS ETHERNET INTERFACE
+echo "Making the Pi have a static IP address for the ethernet interface"
 sudo echo "interface en0" >> /etc/dhcpcd.conf
-sudo echo "static ip_address=192.168.178.122/24" >> /etc/dhcpcd.conf
+sudo echo "static ip_address=192.168.178.144/24" >> /etc/dhcpcd.conf
 sudo echo "static routers=192.168.178.1" >> /etc/dhcpcd.conf
 sudo echo "static domain_name_servers=192.168.178.1 1.1.1.1 8.8.8.8 fd51:42f8:caae:d92e::1" >> /etc/dhcpcd.conf
 
-# TODO COMPILE mqtt-eporter
-# TODO ADD mqtt-eporter bin to cron that executes at boot
-# TODO ADD shell script to cron that grabs sqlite every 5 minutes and uses gnuplot (or alike)
+echo "Install sqlite3"
+sudo apt install sqlite3
 
-echo "Your system should be ready. A restart is required."
-read -p "Press enter to continue"
-sudo shutdown -r now
+echo "Compile and install mqtt-exporter"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git clone https://github.com/rudynorff/pi-energy-monitor.git
+cd ./pi-energy-monitor/mqtt-exporter/
+cargo build --release
+sudo cp ./target/release/mqtt-exporter /usr/bin
+cd ../../raspi-setup/
+sudo cp ./mqtt-exporter.service /etc/systemd/system/
+sudo systemctl enable mqtt-exporter.service
+sudo systemctl start mqtt-exporter.service # start the service directly so we don't have to reboot
+
+# TODO ADD shell script to cron that grabs sqlite every 5 minutes and uses gnuplot (or alike)
+echo "Install and start plotter that creates graphs and shows them as a website"
+
