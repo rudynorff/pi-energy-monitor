fd0c13d89ad79cc8c04e44396584802c43982faa
Rudy Norff <rudy@mactunes.de>
2023-11-13 13:32:07 +0100

    cleaned up install.sh and readme, fix to serviced

--- mqtt-exporter/readme.md
+++ mqtt-exporter/readme.md
@@ -1,26 +1,3 @@
 # mqtt-exporter
 
 mqtt-exporter was built to run on a Raspberry Pi. It connects to an MQTT broker (tested with mosquitto) and subscribes to the topics "solar" and "grid". It then takes these values, stores them in a SQLite database on the Pi.
-
-Every 5 minutes it takes the last 5 minutes worth of data from the SQLite database, puts them in a JSON and sends them off to a web service for further processing.
-
-This is a JSON example:
-
-```
-    {
-        "items": [
-            {
-                "id": "solar",
-                "previous5MinutesWh": 55.2,
-                "currentDayHoursWh": [0, 0, 0, 0, 0, 0, 0, 4.4, 22, 89.2, 212.2, 322, 350, 399, 455, 422, 378, 312, 222, 120, 23, 3, 0, 0, 0],
-                "currentYearKWh": [8, 14, 19, 45, 60, 73, 70, 61, 52, 33, 19, 7],
-            },
-            {
-                "id": "grid",
-                "previous5Minutes": 123.2,
-                "currentDayHours": [0, 0, 0, 0, 0, 0, 0, 990.4, 720, 89.2, 78.2, 77.2, 89.3, 88, 81, 422, 78, 88.3, 332, 800, 312, 333, 278, 0, 0, 0],
-                "currentYearKWh": [411, 399, 340, 322, 278, 266, 269, 289, 311, 333, 378, 401],
-            }
-        ]
-    }
-```
--- raspi-setup/install.sh
+++ raspi-setup/install.sh
@@ -2,14 +2,8 @@
 
 # INSTALL EVERYTHING THAT IS NEEDED
 sudo apt update && sudo apt upgrade
-
-echo "Enabling MosQuiTTo on system start"
 sudo apt install -y mosquitto mosquitto-clients
-sudo touch /etc/mosquitto/conf.d/local.conf
-sudo echo "listener 1883" >> /etc/mosquitto/conf.d/local.conf
-sudo echo "allow_anonymous true" >> /etc/mosquitto/conf.d/local.conf
-sudo systemctl enable mosquitto
-sudo systemctl start mosquitto # start the service directly so we don't have to reboot
+sudo apt install sqlite3
 
 echo "Making the Pi have a static IP address for the ethernet interface"
 sudo echo "interface en0" >> /etc/dhcpcd.conf
@@ -17,8 +11,11 @@ sudo echo "static ip_address=192.168.178.144/24" >> /etc/dhcpcd.conf
 sudo echo "static routers=192.168.178.1" >> /etc/dhcpcd.conf
 sudo echo "static domain_name_servers=192.168.178.1 1.1.1.1 8.8.8.8 fd51:42f8:caae:d92e::1" >> /etc/dhcpcd.conf
 
-echo "Install sqlite3"
-sudo apt install sqlite3
+echo "Enabling MosQuiTTo on system start"
+sudo touch /etc/mosquitto/conf.d/local.conf
+sudo echo "listener 1883" >> /etc/mosquitto/conf.d/local.conf
+sudo echo "allow_anonymous true" >> /etc/mosquitto/conf.d/local.conf
+sudo systemctl enable mosquitto
 
 echo "Compile and install mqtt-exporter"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -29,8 +26,13 @@ sudo cp ./target/release/mqtt-exporter /usr/bin
 cd ../../raspi-setup/
 sudo cp ./mqtt-exporter.service /etc/systemd/system/
 sudo systemctl enable mqtt-exporter.service
-sudo systemctl start mqtt-exporter.service # start the service directly so we don't have to reboot
 
 # TODO ADD shell script to cron that grabs sqlite every 5 minutes and uses gnuplot (or alike)
 echo "Install and start plotter that creates graphs and shows them as a website"
+
+echo "Start services without restarting the system"
+sudo systemctl daemon-reload
+sudo systemctl start mosquitto
+sudo systemctl start mqtt-exporter.service
+# TODO start plotter service
 
